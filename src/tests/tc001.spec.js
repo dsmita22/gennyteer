7f01349b464c9f06df7b71d13693954e704a6eb7
Smita Dutta <dsmita22@gmail.com>
2019-06-12 11:55:10 +1000

    Create tc001.spec.js

---
+++ src/tests/tc001.spec.js
@@ -0,0 +1,14 @@
+const puppeteer = require( 'puppeteer' );
+
+describe( 'Google', () => {
+    it( 'should be titled "Google"', async () => {
+        const args = ['--no-sandbox'];
+        const browser = await puppeteer.launch({
+            args,
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto( 'https://google.com' );
+        await expect( page.title()).resolves.toMatch( 'Google' );
+    });
+});
